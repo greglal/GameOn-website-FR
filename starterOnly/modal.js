ebbc4d3b74260ce490c4121832b823725dd19b49
LAPTOP-LCRCBR4G\glall <g.lallemand@gmail.com>
2023-02-26 22:03:31 +0100

    1st commit - close modal + work in progress on verif form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,24 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+const closeButton = document.querySelector(".close");
+closeButton.addEventListener("click", closeModal);
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+//vérification formulaire
+const testPrenom = document.getElementById("first");
+const testNom = document.getElementById("last");
+
+function validate(event){
+  if(testPrenom.value.length < 2){
+    event.preventDefault();
+    document.getElementById('prenomMissing').innerHTML = "Ce champ doit contenir au moins 2 caracteres" ;
+  } else if(testNom.value.length<1){
+    event.preventDefault();
+    document.getElementById('nomMissing').innerHTML = "Ce champ ne doit pas etre vide" ;
+  }
+}
